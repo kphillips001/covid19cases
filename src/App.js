5ad542b6bd90ea34625a5f282a1cc42a43c4e1e3
Kevin <knphillips01@gmail.com>
2020-05-02 18:50:32 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Cards, CountryPicker, Chart } from './components';
+import styles from './App.module.css';
+import { fetchData } from './api';
+
+class App extends Component {
+  state = {
+    data: {},
+
+  }
+
+  async componentDidMount () {
+    const fetchedData = await fetchData();
+
+    this.setState({ data: fetchedData })
+  }
+
+  render() {
+    const { data } = this.state;
+    return (
+      <div className={styles.container} >
+        <Cards data={ data } />
+        <CountryPicker />
+        <Chart />
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
+
+
+
+
+ 
